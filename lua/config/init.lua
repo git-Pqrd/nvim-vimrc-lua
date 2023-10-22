3d3e24b3ac718343e25d75995e929401e36e773c
francois <francois.piquard11@gmail.com>
2023-10-22 20:33:26 +0100

    Changing name from tpa to config

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,15 +1,11 @@
-require("theprimeagen.set")
-require("theprimeagen.remap")
+require("config.set")
+require("config.remap")
 
 local augroup = vim.api.nvim_create_augroup
-local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+local configGroup = augroup('config', {})
 
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
-
-function R(name)
-    require("plenary.reload").reload_module(name)
-end
 
 autocmd('TextYankPost', {
     group = yank_group,
@@ -23,7 +19,7 @@ autocmd('TextYankPost', {
 })
 
 autocmd({"BufWritePre"}, {
-    group = ThePrimeagenGroup,
+    group = configGroup,
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
